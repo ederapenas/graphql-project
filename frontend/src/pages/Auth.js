3b17b5396951c6ad69eceff27a2b43972435ef84
ederapenas <eder40k@live.com>
2022-11-06 22:10:33 -0300

    upload inicial 2

---
+++ frontend/src/pages/Auth.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class AuthPage extends Component{
+    render(){
+        return (<h1>Pagina de Autentificação</h1>);
+    }
+}
+
+export default AuthPage;
